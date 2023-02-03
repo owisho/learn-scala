579c2ab2cae274d3ae78f19d65176e16670db34f
owisho <owisho2012@126.com>
2023-02-03 08:27:39 +0800

    提交生成螺旋代码

--- src/main/scala/per/owisho/learn/scala/polymorphic/ElementNew.scala
+++ src/main/scala/per/owisho/learn/scala/polymorphic/ElementNew.scala
@@ -10,6 +10,20 @@ abstract class Element{
     elem(
       for((line1,line2) <- this.contents zip that.contents) yield line1+line2
     )
+  def widen(w:Int):Element = 
+    if (w <= width) this
+    else {
+      val left = elem(' ',(w-width)/2,height)
+      val right = elem(' ',w-width-left.width,height)
+      left beside this beside right
+    }
+  def heighten(h:Int):Element = 
+    if (h<height) this 
+    else {
+      val top = elem(' ',width,(h-height)/2)
+      val bot = elem(' ',width,h-height-top.height)
+      top above this above bot
+    }
   override def toString = contents mkString "\n"
 }
 object Element{
@@ -32,6 +46,5 @@ object Element{
     new ArrayElement(contents)
   def elem(ch:Char,width:Int,height:Int):Element = 
     new UniformElement(ch,width,height)
-
-
 }
+
